f944cabf05162873aae165459c1f6ede2f983307
burak subasi <burak.subasi@patikaglobal.com>
2023-03-10 16:19:42 +0300

    update

--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('blogdb', 'root', 'Brksbs5620.', {
-    host: 'https://nodejs-ex-eight.vercel.app',
+    host: 'localhost',
     dialect: 'mysql'
   });
 
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const router = require("./controller/router");
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// let connection = mysql.createConnection({
-//     host:"localhost",
-//     user:"root",
-//     password:"Brksbs5620.",
-//     database:"blogdb"
-// })
-// connection.connect((err)=>{
-//     if(err){
-//         return console.log("connection err",err);
-//     }
-//     console.log("connection success ")
-// })
+let connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Brksbs5620.",
+    database:"blogdb"
+})
+connection.connect((err)=>{
+    if(err){
+        return console.log("connection err",err);
+    }
+    console.log("connection success ")
+})
 app.use("/",router);
 app.listen(4000,()=>{
     console.log("4000 portu dinleniyor")
